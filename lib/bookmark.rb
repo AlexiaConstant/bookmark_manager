0ebb14861eaa380385541557b7996a88a28d9aac
AlexiaConstant <alexia.constant@yahoo.com>
2021-08-11 16:25:25 +0100

    completed chapter 9

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,9 +2,13 @@ require 'pg'
 
 class Bookmark
   def self.all
-    connection = PG.connect(dbname: 'bookmark_database')
-    result = connection.exec('SELECT * FROM bookmarks')
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_database')
+    end
+
+    result = connection.exec("SELECT * FROM bookmarks")
     result.map { |bookmark| bookmark['url'] }
   end
 end
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
+require_relative('./setup_test_database.rb')
+
 ENV['RACK_ENV'] = 'test'
+ENV['ENVIRONMENT'] = 'test'
 
 # Bring in the contents of the `app.rb` file. The below is equivalent to: require_relative '../app.rb'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
@@ -7,6 +10,12 @@ require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+end
 
 # Tell Capybara to talk to BookmarkManager
 Capybara.app = BookmarkManager
